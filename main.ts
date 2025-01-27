f26212775006a21169613eb33a832f8ffd3da504
Katlyn Lorimer <katlyn@katlyn.dev>
2025-01-26 23:53:23 -0900

    chore: lint

--- main.ts
+++ main.ts
@@ -9,7 +9,4 @@ import "$std/dotenv/load.ts";
 import { start } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
 import config from "./fresh.config.ts";
-import { AsyncLocalStorage } from "node:async_hooks";
-import { configure, getConsoleSink } from "@logtape/logtape";
-
 await start(manifest, config);
